f80cb4fc86abdcaeb7b2381cd10382837a269285
mohamed muflahi <muflahimohamed88@gmail.com>
2022-03-16 11:06:51 -0400

    commit tree ac9d17a70c89884dc40bb67cd563403955a087ab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    delimiters = ['.', "?", "!"]
+    x = self.split(Regexp.union(delimiters))
+    puts x
+    y = x.select {|str| str != '' }
+    y.length
+    # x.length
   end
 end
